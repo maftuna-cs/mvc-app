6f7c6ea1860725177e60500c04a25d3f52a3a0ae
Maftuna <kh.maftu@gmail.com>
2020-03-30 10:39:24 +0900

    user model created, user Schema added

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+
+    fistName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    profilePic:{
+        type:String
+    },
+    dateCreated:{
+        type:Date,
+        default:Date.now()
+    }
+
+});
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
